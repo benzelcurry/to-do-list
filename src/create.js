c37e5fb2092ce540928b58ff83a412d9d98d45fe
Ben <benawhiting@gmail.com>
2022-07-29 15:43:46 -0700

    Reorganize task creation code

--- src/create.js
+++ src/create.js
@@ -9,22 +9,45 @@ export default class Task {
     }
 }
 
-export let myTasks = [];
+let myTasks = [];
+let addButton = document.querySelector('.open-form');
+let submitTask = document.querySelector('.btn');
+let cancelButton = document.querySelector('.btn-cancel');
+
+// open + close controls for the task entry popup form
+addButton.addEventListener('click', () => {
+    document.querySelector('.form-popup').style.display = "block";
+});
+  
+cancelButton.addEventListener('click', () => {
+    document.getElementById("myForm").style.display = "none";
+});
+
+// prevent page from refreshing when submit button is hit
+let form = document.querySelector(".form-container");
+
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
 
 export function createTask() {
     let myTasks = [];
-    let taskTitle = "Homework";
-    let taskDescription = "Do homework";
-    let taskDue = "Soon";
-    let taskPriority = "Important";
-    let submitButton = document.querySelector(".btn-submit");
+    let taskTitle = document.querySelector('#task');
+    let taskDescription = document.querySelector('#notes');
+    let taskDue = document.querySelector('#due-date');
+    let taskPriority = document.querySelector('#priority');
+    let submitButton = document.querySelector(".btn");
 
     // WORK ON GETTING CARDS TO ADD TO SCREEN FOR EACH TASK
 
     submitButton.addEventListener('click', () => {
-        const newTask = new Task(taskTitle, taskDescription, taskDue, taskPriority);
+        const newTask = new Task(taskTitle.value, taskDescription.value, taskDue.value, taskPriority.value);
         
         myTasks.push(newTask);
+        taskTitle.value = "";
+        taskDescription.value = "";
+        taskDue.value = "";
+        taskPriority.option = "high";
+        document.getElementById("myForm").style.display = "none";
     });
 
     return myTasks;
--- src/index.js
+++ src/index.js
@@ -2,27 +2,9 @@ import './style.css';
 import Task, { createTask } from './create.js';
 
 let body = document.querySelector('.tasks-display');
-let addButton = document.querySelector('.open-form');
-let submitTask = document.querySelector('.btn');
-let cancelButton = document.querySelector('.btn-cancel');
 let printButton = document.querySelector('.btn-console');
 let submitButton = document.querySelector('.btn-submit');
 let tasks = createTask();
-
-// open + close controls for the task entry popup form
-addButton.addEventListener('click', () => {
-    document.querySelector('.form-popup').style.display = "block";
-});
-  
-cancelButton.addEventListener('click', () => {
-    document.getElementById("myForm").style.display = "none";
-});
-
-// prevent page from refreshing when submit button is hit
-let form = document.querySelector(".form-container");
-
-function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('submit', handleForm);
 
 
 printButton.addEventListener('click', () => {
